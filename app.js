4695bbba4764666d1b393fbe97e2958021086957
David Gaag <davidgaag3@gmail.com>
2023-07-20 13:18:23 -0600

    Add CORS package.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 
 app.locals.pluralize = require('pluralize');
 
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
