0019844cbfd993d2600a9c20496fa7ab76af91f0
David Gaag <davidgaag3@gmail.com>
2023-08-02 15:04:01 -0600

    Re-attempt send username from /login/public-key

--- routes/auth.js
+++ routes/auth.js
@@ -86,9 +86,9 @@ router.post('/login/public-key', passport.authenticate('webauthn', {
   //
   console.log("In /login/public-key after passport.authenticate. Request body:");
   console.log(req.body)
-  console.log("req.username: " + req.username)
+  console.log("req.user: " + req.user)
   //
-  res.json({ ok: true, username: req.username }); // TODO: encode username in base64url?
+  res.json({ ok: true, username: req.user.username }); // TODO: encode username in base64url?
 }, function(err, req, res, next) {
   //
   console.log("In /login/public-key after passport.authenticate ERROR FUNC. Request body:");
