bf9c854a9d331927766803f2734bed8c39a45767
David Gaag <davidgaag3@gmail.com>
2023-08-10 13:33:22 -0600

    Secure session cookie, implement endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,15 @@
 var express = require('express');
-var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var pool = require('../db');
 
-var ensureLoggedIn = ensureLogIn();
-
-function fetchTodos(req, res, next) {
-  pool.query('SELECT * FROM todos WHERE owner_id = $1', [
-    req.user.id
-  ], function(err, result) {
-    if (err) { return next(err); }
-    
-    const rows = result.rows;
-
-    var todos = rows.map(function(row) {
-      return {
-        id: row.id,
-        title: row.title,
-        completed: row.completed == 1 ? true : false,
-        url: '/' + row.id
-      }
-    });
-    res.locals.todos = todos;
-    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
-    res.locals.completedCount = todos.length - res.locals.activeCount;
-    next();
-  });
-}
-
 var router = express.Router();
-
-router.get('/checkAuthentication', function(req, res, next) {
-  console.log("/checkAuthentication - User:" + req.user);
+router.use('/', secureRouter); // TODO: Path
+var secureRouter = express.Router(); // TODO: Merge params?
+secureRouter.use((req, res, next) => {
+  console.log("index.js auth router middleware - User:" + req.user);
   if (req.isAuthenticated()) {
-    res.sendStatus(200);
+    next();
   } else {
-    res.sendStatus(401)
+    res.sendStatus(401);
   }
 });
 
@@ -47,87 +22,121 @@ router.get('/', function(req, res, next) {
   res.render('index', { user: req.user });
 });
 
-router.get('/active', ensureLoggedIn, fetchTodos, function(req, res, next) {
-  res.locals.todos = res.locals.todos.filter(function(todo) { return !todo.completed; });
-  res.locals.filter = 'active';
-  res.render('index', { user: req.user });
+secureRouter.get('/checkAuthentication', function(req, res, next) {
+  console.log("/checkAuthentication - User:" + req.user);
+  if (req.isAuthenticated()) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401)
+  }
 });
 
-router.get('/completed', ensureLoggedIn, fetchTodos, function(req, res, next) {
-  res.locals.todos = res.locals.todos.filter(function(todo) { return todo.completed; });
-  res.locals.filter = 'completed';
-  res.render('index', { user: req.user });
+// Get info on current user
+secureRouter.get('/me', function(req, res, next) {
+  const profile = pool.getProfileByID(req.user.id);
+  if (profile == null) {
+    res.sendStatus(500);
+  } else if (profile === undefined) {
+    res.sendStatus(404);
+  }
+
+  const numFriends = getFriendsByID(req.user.id).length;
+
+  const json = {
+    "profile": {
+      "username": req.user.username,
+      "first_name": profile.first_name,
+      "last_name": profile.last_name,
+      "display_name": profile.first_name + " " + profile.last_name,
+      "relationship": "me",
+      "friends_count": numFriends, 
+      "id": req.user.id,
+      "balance": profile.balance
+    }
+  }
+  res.status(200).json(json);
 });
 
-router.post('/', ensureLoggedIn, function(req, res, next) {
-  req.body.title = req.body.title.trim();
-  next();
-}, function(req, res, next) {
-  if (req.body.title !== '') { return next(); }
-  return res.redirect('/' + (req.body.filter || ''));
-}, function(req, res, next) {
-  pool.query('INSERT INTO todos (owner_id, title, completed) VALUES ($1, $2, $3)', [
-    req.user.id,
-    req.body.title,
-    req.body.completed == true ? 1 : null
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
+// Get info on a user
+router.get('/profiles/:user_id', function(req, res, next) {
+  const profile = pool.getProfileByID(req.params.user_id);
+  if (profile == null) {
+    res.sendStatus(500);
+  } else if (profile === undefined) {
+    res.sendStatus(404);
+  }
+
+  let relationship;
+  if (req.isAuthenticated()) {
+    const relationshipRow = pool.getRelationshipRow(req.user.id, req.params.user_id);
+    if (relationshipRow == null) {
+      res.sendStatus(500);
+    } else if (relationshipRow === undefined) {
+      relationship = "none";
+    } else {
+      relationship = relationshipRow.relationship;
+    }
+  } else {
+    relationship = "unknown";
+  }
+
+  const numFriends = getFriendsByID(req.params.user_id)?.length; // TODO: error handling
+
+  const json = {
+    "profile": {
+      "username": profile.username,
+      "first_name": profile.first_name,
+      "last_name": profile.last_name,
+      "display_name": profile.first_name + " " + profile.last_name,
+      "relationship": relationship,
+      "friends_count": numFriends,
+      "id": req.user.id
+    }
+  }
 });
 
-router.post('/:id(\\d+)', ensureLoggedIn, function(req, res, next) {
-  req.body.title = req.body.title.trim();
-  next();
-}, function(req, res, next) {
-  if (req.body.title !== '') { return next(); }
-  pool.query('DELETE FROM todos WHERE id = $1 AND owner_id = $2', [
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-}, function(req, res, next) {
-  pool.query('UPDATE todos SET title = $1, completed = $2 WHERE id = $3 AND owner_id = $4', [
-    req.body.title,
-    req.body.completed !== undefined ? 1 : null,
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
+// Get friends of a user
+router.get('/profiles/:user_id/friends', function(req, res, next) {
+  const friends = pool.getFriendsByID(req.params.user_id);
+  if (friends == null) {
+    res.sendStatus(404);
+  }
+
+  const json = { 
+    "friends": []
+  }
+
+  for (const friend in friends) {
+    json.friends.push({
+      "username": friend.username,
+      "first_name": friend.first_name,
+      "last_name": friend.last_name,
+      "display_name": friend.first_name + " " + friend.last_name,
+      "id": friend.id 
+    });
+  }
 });
 
-router.post('/:id(\\d+)/delete', ensureLoggedIn, function(req, res, next) {
-  pool.query('DELETE FROM todos WHERE id = $1 AND owner_id = $2', [
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
+// Payments/Transactions
+
+// Make a payment/charge
+secureRouter.post('/payments', function(req, res, next) {
+  
 });
 
-router.post('/toggle-all', ensureLoggedIn, function(req, res, next) {
-  pool.query('UPDATE todos SET completed = $1 WHERE owner_id = $2', [
-    req.body.completed !== undefined ? 1 : null,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
+// List recent payments/charges
+secureRouter.get('/payments', function(req, res, next) {
+
 });
 
-router.post('/clear-completed', ensureLoggedIn, function(req, res, next) {
-  pool.query('DELETE FROM todos WHERE owner_id = $1 AND completed = $2', [
-    req.user.id,
-    1
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
+// Get info on a payment/charge
+secureRouter.get('/payments/:payment_id', function(req, res, next) {
+
 });
 
-module.exports = router;
+// Complete a payment/charge request
+secureRouter.put('/payments/:payment_id', function(req, res, next) {
+
+});
+
+module.exports = secureRouter;
