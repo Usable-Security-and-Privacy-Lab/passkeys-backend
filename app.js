29d67f9c39e246e411bd08ead3df9d13d7fc22d7
David Gaag <davidgaag3@gmail.com>
2023-08-17 13:00:23 -0600

    More logging

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err) // TODO: remove in production
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
